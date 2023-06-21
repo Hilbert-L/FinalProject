4e93860056c6738f8edfe0c2a361cfb2c34bf0c0
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-06-21 22:44:15 +1000

    chore: remove old frontend

--- frontend/src/pages/AdminLogin.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const AdminLogin = () => {
-  return (
-    <div>
-      <h1>Admin Login</h1>
-    </div>
-  );
-};
--- frontend/src/pages/UserSearch.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const UserSearch = () => {
-  return (
-    <div>
-      <h1>User Search</h1>
-    </div>
-  );
-};
