a3fe54ade6077cb71159be1f361d4591ff294a16
Hilbert-L <57756073+Hilbert-L@users.noreply.github.com>
2023-07-28 15:33:23 +1000

    Update CarSpaceRouters.py
    Split Image upload from create car space

--- backend/routers/CarSpaceRouters.py
+++ backend/routers/CarSpaceRouters.py
@@ -47,20 +47,11 @@ def is_valid_image(base64_img_str):
 
 @CarSpaceRouter.post("/carspace/create_car_space", tags=["Car Spaces"])
 @check_token
-async def create_car_space(create_car_space: CreateCarSpaceSchema, base64_image: str = None,
-                           token: str = Depends(verify_user_token)):
+async def create_car_space(create_car_space: CreateCarSpaceSchema,token: str = Depends(verify_user_token)):
     stored_user = users_collections.find_one({"username": token})
 
     if stored_user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid user")
-
-    if not base64_image:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No image provided")
-
-    if not is_valid_image(base64_image):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid image format")
-
-
 
 
     num_car_spaces = car_space_id_collections.find_one({"_id": ObjectId("64ba8947a0d25b4ae8a3cd84")})
@@ -89,11 +80,38 @@ async def create_car_space(create_car_space: CreateCarSpaceSchema, base64_image:
     )
 
     new_car_space_dict = new_car_space.dict()
-    new_car_space_dict["image"] = base64_image
     car_space_collections.insert_one(dict(new_car_space_dict))
     num_car_spaces['id'] += 1
     car_space_id_collections.update_one({"_id": ObjectId("64ba8947a0d25b4ae8a3cd84")}, {"$set": {"id": num_car_spaces["id"]}})
     return {"Message": "Car Space Added Successfully", "Car Space": new_car_space_dict}
+
+@CarSpaceRouter.post("/carspace/upload_carspace_image/{car_space_id}", tags=["Car Spaces"])
+@check_token
+async def upload_carspace_image(car_space_id: int,image: UploadFile = File(...),token: str = Depends(verify_user_token)):
+    stored_user = users_collections.find_one({"username": token})
+
+    if stored_user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid user")
+
+    stored_car_space = car_space_collections.find_one({"carspaceid": car_space_id})
+
+    if stored_car_space is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Car space {car_space_id} not found")
+
+    contents = await image.read()
+    base64_image = base64.b64encode(contents).decode()
+
+    if not is_valid_image(base64_image):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid image format")
+
+    image_uri = f"data:{image.content_type};base64,{base64_image}"
+
+    car_space_collections.update_one(
+        {"carspaceid": car_space_id},
+        {"$set": {"image": image_uri}}
+    )
+
+    return {"Message": "Image uploaded successfully"}
 
 
 # Will add three attributes: leasing, using and booking
